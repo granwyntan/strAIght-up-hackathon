f34db44b81815c484e861c8a309ea8eb82782041
neudheh <chuakai07548@gmail.com>
2026-04-19 15:42:01 +0800

    updated image api, granwyn can pretty much work on website alr

--- request.py
+++ request.py
@@ -3,10 +3,10 @@ import requests, base64, os
 image = os.path.join("uploads", "638177-1.png")
 
 with open(image, "rb") as file:
-    encoded = base64.b64encode(file.read())
+    encoded = base64.b64encode(file.read()).decode("utf-8")
 
 encoded = str(encoded)
 
 
-# r = requests.post("http://127.0.0.1:8000/scan_meds", json={"msg": encoded})
-# print(r.json()["response"])
+r = requests.post("http://127.0.0.1:8000/scan_meds", json={"msg": encoded})
+print(r.json()["response"])
